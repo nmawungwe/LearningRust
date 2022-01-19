cfcce890481d96737c0f2d834e6394fd0d47f2e0
Nyasha <nmawungwe@gmail.com>
2022-01-19 16:33:53 +0200

    learnt about variables

---
+++ variables/target/debug/deps/variables-3f07c1cc04363f9a.d
@@ -0,0 +1,5 @@
+/Users/nyashamawungwe/Documents/Projects/LearningRust/variables/target/debug/deps/variables-3f07c1cc04363f9a: src/main.rs
+
+/Users/nyashamawungwe/Documents/Projects/LearningRust/variables/target/debug/deps/variables-3f07c1cc04363f9a.d: src/main.rs
+
+src/main.rs:
