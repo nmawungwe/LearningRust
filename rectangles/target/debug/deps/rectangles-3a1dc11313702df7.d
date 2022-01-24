a6c94b384ad638b8607b4968eadd2f1ab531ede7
Nyasha <nmawungwe@gmail.com>
2022-01-24 12:17:38 +0200

    learnt how structs work and attaching methods to them

---
+++ rectangles/target/debug/deps/rectangles-3a1dc11313702df7.d
@@ -0,0 +1,5 @@
+/Users/nyashamawungwe/Documents/Projects/LearningRust/rectangles/target/debug/deps/rectangles-3a1dc11313702df7: src/main.rs
+
+/Users/nyashamawungwe/Documents/Projects/LearningRust/rectangles/target/debug/deps/rectangles-3a1dc11313702df7.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/Users/nyashamawungwe/Documents/Projects/LearningRust/rectangles/target/debug/rectangles: /Users/nyashamawungwe/Documents/Projects/LearningRust/rectangles/src/main.rs
